caf21d893ff85eb0e9624d84207042f7b785aea4
Firat Kusaslan <firatkusaslann@gmail.com>
2022-03-27 14:21:06 +0300

    atama karşılaştırma and ve pipe

---
+++ conditionals2.js
@@ -0,0 +1,29 @@
+/*
+var sayi1 = 10;
+var sayi2 = 20;
+
+if (sayi1 == sayi2) {
+  console.log("Eşit");
+}
+*/
+
+/*
+
+//and "&&" kullanımı ikiden fazla karşılaştırma yapılacaksa kullanılır.
+var sayi1 = 10;
+var sayi2 = 10;
+var sayi3 = 10;
+
+if (sayi1 == sayi2 && sayi1 == sayi3) {
+  console.log("Eşit");
+}
+*/
+
+//veya "||" kullanımı ikisinden birinisini karşılaştırma yapılacaksa kullanılır.
+var sayi1 = 10;
+var sayi2 = 10;
+var sayi3 = 20;
+
+if (sayi1 == sayi2 || sayi1 == sayi3) {
+  console.log("Eşit");
+}
