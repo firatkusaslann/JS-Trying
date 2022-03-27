04d4c3274b48c2572624de153bf243fc7c78cc2d
Firat Kusaslan <firatkusaslann@gmail.com>
2022-03-27 15:19:20 +0300

    for döngüsü

---
+++ for.js
@@ -0,0 +1,9 @@
+/*
+for (var i = 1; i <= 10; i++) {     //1den 10a kadar tüm sayıalr
+  console.log(i);
+}
+*/
+
+for (var i = 2; i <= 100; i = i + 2) {      //2den 100e kadar çift sayılar
+  console.log(i);
+}
