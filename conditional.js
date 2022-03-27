1b4c311e6ff205cba8ac49e7e09efe577eecddca
Firat Kusaslan <firatkusaslann@gmail.com>
2022-03-27 13:46:33 +0300

    if else ve else if issue

--- conditional.js
+++ conditional.js
@@ -1,1 +1,31 @@
-// Şart Blokları 
+// Şart Blokları
+
+var not = 46;
+
+/*
+if (not <= 50) {
+  console.log("Geçemedi");
+}
+
+if (not > 50) {
+  console.log("Geçtiniz");
+}
+*/
+
+//başka bir kullanım
+
+/*
+if (not <= 50) {
+  console.log("Geçemedi");
+} else {
+  console.log("kaldınız");
+}
+*/
+
+if (not > 50) {
+  console.log("Geçtiniz");
+} else if (not >= 40) {
+  console.log("Bütünlemeye kaldiniz");
+} else {
+  console.log("Gecemediniz");
+}
