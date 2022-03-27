e6ad695b5813a07634258088af37a250738ff1a3
Firat Kusaslan <firatkusaslann@gmail.com>
2022-03-27 15:08:29 +0300

    while döngüsü

---
+++ while.js
@@ -0,0 +1,28 @@
+// var sayi = 10;
+/*
+while (sayi <= 10) {        //infinite loop
+  console.log(sayi);
+}
+
+while (sayi <= 10) {       //infinite loop
+  console.log(sayi);
+  sayi = sayi - 1;
+}
+
+*/
+
+/*
+var sayi = 1;
+
+while (sayi <= 10) {
+  // 1 den 10 a kadar olan sayıları yazdır
+  console.log(sayi);
+  sayi = sayi + 1;
+}
+*/
+
+var sayi = 2;
+while (sayi <= 100) {
+  console.log(sayi);
+  sayi = sayi + 2;
+}
