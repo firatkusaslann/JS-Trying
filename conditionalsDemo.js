fb9ba82ac0b046645a501f11627b4c44af08c5d7
Firat Kusaslan <firatkusaslann@gmail.com>
2022-03-27 14:06:37 +0300

    büyük sayıyı bulma

---
+++ conditionalsDemo.js
@@ -0,0 +1,17 @@
+var sayi1 = 2321;
+var sayi2 = 14342;
+var sayi3 = 54;
+
+// en büyük sayıyı bulunuz
+
+var enBuyuk = sayi1; //29
+
+if (enBuyuk < sayi2) {
+  enBuyuk = sayi2; //30
+}
+
+if (enBuyuk < sayi3) {
+  enBuyuk = sayi3;
+}
+
+console.log("Bu sayıların içinde en büyük = " + enBuyuk);
