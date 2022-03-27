352d2f2dafd5bc94e4cdff115c2d3670ca76d438
Firat Kusaslan <firatkusaslann@gmail.com>
2022-03-27 12:07:12 +0300

    tekrar

---
+++ day2.js
@@ -0,0 +1,5 @@
+hamburger = "Whooper"
+icecek = "Cola"
+console.log(hamburger)
+console.log(icecek)
+console.log(hamburger + " " + icecek)
---
+++ variables.js
@@ -0,0 +1,12 @@
+var hamburger = "whooper" // string bir veri türü (metinsel)
+console.log(hamburger)
+console.log(typeof (hamburger))
+
+hamburger = 10             // number bir veri türü (sayısal)
+console.log(hamburger)
+console.log(typeof (hamburger))
+
+var aboneMi = false         // boolean bir veri türüdür (Mantıksal) karşılığı true ya da false'dur.
+console.log(aboneMi)
+console.log(typeof (aboneMi))
+
