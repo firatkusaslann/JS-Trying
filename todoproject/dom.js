4874a09f9453559a91a28043554c4ccab46e9159
Firat Kusaslan <firatkusaslann@gmail.com>
2022-03-31 20:46:30 +0300

    dom part 1 finish

---
+++ todoproject/dom.js
@@ -0,0 +1,33 @@
+value = document;
+value = document.all;
+value = document.all.length;
+value = document.all[0];
+value = document.all[6];
+value = document.all[document.all.length - 1];
+
+const elements = document.all; // HTML COLLECTİON
+// for (let i = 0; i < elements.length; i++) {
+//   console.log(elements[i]);
+// }
+
+// elements.forEach(function (element) {
+//   console.log(element);
+// });
+// console.log(value);
+
+// const collections = Array.from(document.all);
+// console.log(typeof collections);
+
+// collections.forEach(function (collection) {
+//   console.log(collection);
+// });
+
+value = document.all[8];
+value = document.body;
+value = document.head;
+value = document.location;
+value = document.location.hostname;
+value = document.location.port;
+value = document.URL;
+value = document.characterSet;
+console.log(value);
