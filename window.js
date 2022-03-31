f3bcf4268afcdb69c086289919d7aec45e1a08d8
Firat Kusaslan <firatkusaslann@gmail.com>
2022-03-31 15:12:58 +0300

    window objesi

---
+++ window.js
@@ -0,0 +1,54 @@
+// Window objesine genel bakış
+
+// alert("merhaba");
+// console.log(this);
+
+/////////////////Confirm işlemi
+
+// const cevap = confirm("Emin misiniz?");
+// console.log(cevap);
+// if (cevap) {
+//   console.log("Silin Gitsin");
+// } else {
+//   console.log("Silinmedi");
+// }
+
+//Başka bir kulllanım
+
+// if (confirm("Emin misiniz?")) {
+//   console.log("Silin Gitsin");
+// } else {
+//   console.log("Silinmedi");
+// }
+
+// const cevap = prompt("2+2=?");
+
+// if (cevap == 4) {
+//   console.log("Doğru Cevap");
+// } else {
+//   console.log("Yanlış Cevap");
+// }
+
+let value;
+
+value = window;
+value = window.location;
+value = window.location.host;
+value = window.location.hostname;
+value = window.location.port;
+value = window.location.href;
+
+// sayfayı sonsuz kez yeniler------------value = window.location.href;
+
+// if (confirm("Sayfa yenilensi mi")) {
+//   window.location.reload();
+// } else {
+//   console.log("Sayfa Yenilenmedi");
+// }
+
+value = window.outerHeight; // pencere yüksekliği
+valuee = window.outerWidth; // pencere genişliği
+value = window.innerHeight; // Sayfa yüksekliği
+valuee = window.innerWidth; // Sayfa geniliği
+
+console.log("Yükseklik: " + value, " Genişlik: " + valuee);
