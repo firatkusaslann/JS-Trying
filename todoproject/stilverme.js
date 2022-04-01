57583de52b5f3aaebb4940bd9b8f62882630a7fe
Firat Kusaslan <firatkusaslann@gmail.com>
2022-04-01 19:39:18 +0300

    element özellikleri

---
+++ todoproject/stilverme.js
@@ -0,0 +1,14 @@
+const element = document.querySelector("#clear-todos");
+
+// element özellikleri
+
+/*
+console.log(element.id);
+console.log(element.className);
+console.log(element.classList);
+console.log(element.classList[1]);
+console.log(element.textContent);
+console.log(element.innerHTML);
+console.log(element.href);
+console.log(element.style);
+*/
