2ab528a6429434c9ca87fd6d3335913abd8b6855
Firat Kusaslan <firatkusaslann@gmail.com>
2022-04-04 01:09:00 +0300

    dinamik element silme  remove metodu

---
+++ todoproject/desilme.js
@@ -0,0 +1,38 @@
+// // dinamik element silme
+
+const todoList = document.querySelector("ul.list-group");
+const todos = document.querySelectorAll("li.list-group-item");
+
+// todos.forEach(function (todo) {
+//     todo.remove();
+
+// });
+
+for (let i = 0; todos.length > i; i++) {
+  todos[i].remove();
+}
+
+// // Remove Metodu
+
+// todos[0].remove();
+// // todoList.removeChild(todoList.lastElementChild);
+
+console.log(todo);
+// console.log(todoList);
+
+// Dinamik Element Silme
+
+// const todoList = document.querySelector("ul.list-group");
+// const todos = document.querySelectorAll("li.list-group-item");
+
+// // Remove Metodu
+
+// todos[1].remove();
+
+// // Remove Child
+
+// // todoList.removeChild(todoList.lastElementChild);
+// // todoList.removeChild(todos[3]);
+
+// console.log(todos);
+// console.log(todoList);
