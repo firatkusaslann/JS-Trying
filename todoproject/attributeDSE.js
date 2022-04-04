7296e68539445832e4cf0d2944d2e91c878c4a65
Firat Kusaslan <firatkusaslann@gmail.com>
2022-04-04 19:56:50 +0300

    attribüte silme değiştirme ekleme

---
+++ todoproject/attributeDSE.js
@@ -0,0 +1,58 @@
+// const todoInput = document.getElementById("todo");
+// let element;
+// element = todoInput;
+// element = todoInput.classList;
+
+// // todoInput.classList.add("new-class"); //ekleme
+// // todoInput.classList.add("new-class2"); // ekleme
+// // todoInput.classList.remove("form-control"); // çıkarma & kaldırma
+
+// element = todoInput;
+// element = todoInput.placeholder;
+// element = todoInput.getAttribute("placeholder"); // uzun kullanım
+// todoInput.setAttribute("placeholder", "Bir değer giriniz"); // attribute değiştirme ve ekleme
+// todoInput.setAttribute("title", "Input"); // atribute değiştirme ve ekleme
+// element = todoInput;
+// element = todoInput.hasAttribute("required"); // var mı sorgusu
+// element = todoInput.hasAttribute("name");
+// element = todoInput.remove("name");
+// element = todoInput;
+// console.log(element);
+
+// const todoInput = document.getElementById("todo");
+// let element;
+// element = todoInput;
+// element = todoInput.classList;
+
+// todoInput.classList.add("addingClass");
+// todoInput.classList.add("addingClass2");
+// todoInput.classList.remove("addingClass", "addingClass2"); // dikkat a
+
+// element = todoInput;
+// element = todoInput.placeholder;
+// element = todoInput.getAttribute("placeholder");
+// todoInput.setAttribute("placeholder", "Burada bir input var");
+// todoInput.setAttribute("title", "title eklendi");
+// todoInput.removeAttribute("name");     /// dikkat a
+// element = todoInput;
+// // element = todoInput.hasAttribute("name"); //true
+// // element = todoInput.hasAttribute("name2"); //false
+// console.log(element);
+
+const clear = document.getElementById("clear-todos");
+let btn;
+btn = clear;
+btn = clear.classList;
+
+clear.classList.add("adding1", "adding2");
+clear.classList.remove("adding1", "adding2");
+
+btn = clear;
+btn = clear.textContent;
+
+
+
+
+
+
+console.log(btn);
