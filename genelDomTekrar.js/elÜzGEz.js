1b204f8cc054a5fcf57c2f0e6731766e986a2732
Firat Kusaslan <firatkusaslann@gmail.com>
2022-04-07 21:06:40 +0300

    ...

---
+++ genelDomTekrar.js/elÜzGEz.js
@@ -0,0 +1,10 @@
+let value;
+const todoList = document.querySelector(".list-group");
+const todo = document.querySelector(".list-group-item:nth-child(2)");
+const cardrow = document.querySelector(".card.row");
+
+value = todoList;
+value = todo;
+value = cardrow;
+
+console.log(value);
