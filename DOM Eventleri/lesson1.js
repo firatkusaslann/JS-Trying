b12ef224628d957119a9a17d0d702aab0862a5e9
Firat Kusaslan <firatkusaslann@gmail.com>
2022-04-08 23:39:47 +0300

    DOM eventleri başlangıç

---
+++ DOM Eventleri/lesson1.js
@@ -0,0 +1,24 @@
+// const filterInput = document.getElementById("filter");
+
+// // filterInput.onfocus = function () {   // eski ve pek kullanılmayan bir yöntem
+// //   console.log("Naber");
+// // };
+
+// filterInput.addEventListener("focus", function (e) {
+//   console.log(e);
+//   console.log(e.type);
+//   console.log(e.target);
+//   console.log(e.target.placeholder);
+//   console.log(e.target.className);
+//   //   console.log("Naber");
+// });
+
+const todoForm = document.getElementById("todo-form");
+
+todoForm.addEventListener("submit", submitForm);
+
+function submitForm(e) {
+  console.log("Submit Eventi"); // submitin yenileme özelliğini etkisiz kılar
+
+  e.preventDefault();
+}
