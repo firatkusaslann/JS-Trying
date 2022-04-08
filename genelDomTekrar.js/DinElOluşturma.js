41ad653d598b2f6c947c9595c265ad81c8d961bb
Firat Kusaslan <firatkusaslann@gmail.com>
2022-04-08 21:49:52 +0300

    finish

--- genelDomTekrar.js/DinElOluşturma.js
+++ genelDomTekrar.js/DinElOluşturma.js
@@ -8,6 +8,12 @@ newLink.className = "btn btn-danger";
 newLink.href = "https://www.google.com";
 newLink.target = "_blank";
 
+newLink.appendChild(document.createTextNode("Farklı sayfaya git"));
+cardbody.appendChild(newLink);
+
 // newLink.textContent = "Buraya Basın"; // tüm içeriği silip girilen değeri yazar. pek doğru bir kullanım değil
+// const text = document.createTextNode("N'aber?");
+// cardbody.appendChild(text);
+// console.log(cardbody);
 
 console.log(newLink);
