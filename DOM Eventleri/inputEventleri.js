5c42b409fa8ed0975d09da0f92adf892ce347b19
Firat Kusaslan <firatkusaslann@gmail.com>
2022-04-09 18:47:57 +0300

    İnput eventleri

---
+++ DOM Eventleri/inputEventleri.js
@@ -0,0 +1,15 @@
+//            // DOMContentLoaded Event'i
+// document.addEventListener("DOMContentLoaded", load);
+// function load(e) {
+//   console.log("Sayfa Yüklendi");
+// }
+
+const filter = document.querySelector("#filter");
+
+// // Focus -- mouse ve tab ile odaklanınca
+// filter.addEventListener("focus", run);
+// // Blur -- mouse ve tab ile odaktan çıkınca
+// filter.addEventListener("blur", run);
+// function run(e) {
+//   console.log(e.type);
+// }
