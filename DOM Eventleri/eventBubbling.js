b4cf4b57744fc787a42b74fb84b9a725ad2535ca
Firat Kusaslan <firatkusaslann@gmail.com>
2022-04-09 20:01:05 +0300

    Event Bubbling

---
+++ DOM Eventleri/eventBubbling.js
@@ -0,0 +1,13 @@
+// Event Bubbling
+
+document.querySelector(".container").addEventListener("click", function () {
+  console.log("Div Container");
+});
+
+document.querySelector(".card.row").addEventListener("click", function () {
+  console.log("Card Row");
+});
+
+document.querySelectorAll(".card-body")[1].addEventListener("click", function () {
+  console.log("Card Body");
+});
